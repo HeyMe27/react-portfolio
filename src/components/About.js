5b80cbdf1c46498879f513173867ed291887cbee
Hemanth Gowda K S <kshemanth27@gmail.com>
2025-08-03 14:39:28 +0530

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section style={{ padding: '2rem' }}>
+      <h2>About Me</h2>
+      <p>I currently work as a Full-stack Developer at TCS, handling GE Healthcare web apps.</p>
+    </section>
+  );
+}
+
+export default About;
